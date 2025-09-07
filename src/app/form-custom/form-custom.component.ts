3e26ba58a278ff7782d0ea09e441ee0450bf9924
Vitalii Soloninko <130390124+VitaliiSoloninko@users.noreply.github.com>
2025-09-07 17:06:05 +0200

    feat: add totalQuantity to a form

--- src/app/form-custom/form-custom.component.ts
+++ src/app/form-custom/form-custom.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { ChooseQuantityComponent } from '../choose-quantity/choose-quantity.component';
 
 @Component({
   selector: 'app-form-custom',
-  imports: [ChooseQuantityComponent],
+  imports: [
+    ChooseQuantityComponent,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
   templateUrl: './form-custom.component.html',
   styleUrl: './form-custom.component.scss',
 })
-export class FormCustomComponent {}
+export class FormCustomComponent implements OnInit {
+  form!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      title: [''],
+      totalQuantity: [60, [Validators.required, Validators.max(100)]],
+    });
+  }
+
+  submit() {
+    console.log(this.form.value);
+    this.form.reset({
+      title: '',
+      totalQuantity: '',
+    });
+  }
+}
